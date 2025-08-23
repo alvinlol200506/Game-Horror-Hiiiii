450931eb1b86bba2c83e8eeeedd4b5a7a017196e
alvin <alvinsatria452@gmail.com>
2025-08-23 22:44:26 +0700

    Respawn system

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -5,13 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
+    public string saveKey = "IsJumpscareKey";
+    public Transform jumpScareCheckPoint;
+    public Transform player;
     public TMP_Text GameOverText;
     public string gameOverMessage = "IHHHH TAKOTNYEEE";
 
-    private IEnumerator Start()
+    [Header("Debug")]
+    public int isJumpscareSaved = 0;
+
+    private void Awake()
+    {
+        isJumpscareSaved = PlayerPrefs.GetInt(saveKey);
+
+        if (isJumpscareSaved == 1)
+        {
+            player.position = jumpScareCheckPoint.position;
+            player.rotation = jumpScareCheckPoint.rotation;
+        }
+    }
+    public void StartJumpscare()
+    {
+        StartCoroutine(StartJumpscareDelay());
+    }
+    IEnumerator StartJumpscareDelay()
     {
         GameOverText.gameObject.SetActive(true);
         GameOverText.text = gameOverMessage;
+
+        isJumpscareSaved = 1;
+        PlayerPrefs.SetInt(saveKey, isJumpscareSaved);
+        PlayerPrefs.Save();
 
         yield return new WaitForSeconds(2);
 
