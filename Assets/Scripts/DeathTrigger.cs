d40ebd2de2a5611297809c449221d9dc84e54974
alvin <alvinsatria452@gmail.com>
2025-08-23 08:24:10 +0700

    Create death and a room

--- Assets/Scripts/DeathTrigger.cs
+++ Assets/Scripts/DeathTrigger.cs
@@ -1,7 +1,26 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathTrigger : MonoBehaviour
 {
-    public TMP_Text;
+    public TMP_Text deathText;
+    public string deathMessage = "MATIIII!!!!";
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            deathText.gameObject.SetActive(true);
+            deathText.text = deathMessage;
+
+            StartCoroutine(RestartDelay());
+        }
+    }
+
+    private IEnumerator RestartDelay()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
